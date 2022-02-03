c83baa9a129896e8bd71e6babc57e7d2aac21373
Natasha Koller <kollernatasha@gmail.com>
2022-02-03 09:02:40 -0500

    updates

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,36 @@
 import React from "react";
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>Put the bio in here
+       
+      </p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <user links={props.links}></user>
     </div>
   );
 }
 
 export default About;
+const pTag (){
+ if (!props.isPassedDown) {
+    // display in p tag
+          return null 
+  } else {
+    // don't display p tag
+   return (
+        <div>
+        <p>{props.bio}</p>
+        </div>
+          )
+  }
+}
+const pTag = props.isPassedDown
+if (isPassedDown) {
+  return { props.bio }
+} else {
+   return null
+}
---
+++ src/components/Links.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Links(props) {
+    <div>
+    <h3>Links</h3>
+    <a href={props.links.linkedin}/>
+    <a href={props.links.github}/>
+    </div>
+}
+export default Links
